8f938199d8a6441fe2b8bf0102e57e5669629037
Fialco <113672867+Fialco@users.noreply.github.com>
2025-11-10 19:09:38 +0200

    1.11 persisting data

--- log_output/log-provider/index.js
+++ log_output/log-provider/index.js
@@ -5,13 +5,14 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (request, response) => {
-  fs.readFile("files/log.txt", "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    response.send(data);
-  });
+  const logs = fs.readFileSync("files/log.txt", "utf8");
+  const pongs = fs.readFileSync("files/pongs.txt", "utf8");
+
+  const lines = logs.split("\n");
+  const last = lines[lines.length - 2];
+
+  const page = `<div><p>${last}</p><p>Ping / Pongs: ${pongs}</p></div>`;
+  response.send(page);
 });
 
 const PORT = process.env.PORT || 3001;
--- ping-pong-app/index.js
+++ ping-pong-app/index.js
@@ -1,13 +1,26 @@
 const express = require("express");
+const fs = require("node:fs");
 
 const app = express();
 app.use(express.json());
 
-let pongs = 0;
+const pongsPath = "./files/pongs.txt";
 
-app.get("/pingpong", (request, response) => {
-  const pongsNow = pongs++;
-  response.send(`<p>pong ${pongsNow}</p>`);
+const getPongCount = () => {
+  if (fs.existsSync(pongsPath)) {
+    const data = fs.readFileSync(pongsPath, "utf8");
+    return parseInt(data);
+  }
+  return 0;
+};
+
+app.get("/pingpong", (req, response) => {
+  let pongs = getPongCount();
+  pongs++;
+
+  fs.writeFileSync(pongsPath, pongs.toString());
+
+  response.send(`<p>pong ${pongs}</p>`);
 });
 
 const PORT = process.env.PORT || 3001;
